35b7a937789243817b7fc52593cb92d781211280
juunhyuukk <65065716+juunhyuukk@users.noreply.github.com>
2023-01-11 15:23:33 +0900

    [D1] Title: 1대1 가위바위보, Time: 144 ms, Memory: 58,484 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,14 @@
+A, B = list(map(int,input().split()))
+
+if A == 1 and B == 2 :
+    print("B")
+elif A == 1 and B == 3 :
+    print("A")
+elif A == 2 and B == 1 :
+    print("A")
+elif A == 2 and B == 3 :
+    print("B")
+elif A == 3 and B == 1 :
+    print("B")
+elif A == 3 and B == 2 :
+    print("A")
