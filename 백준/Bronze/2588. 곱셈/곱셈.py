28026b54e8fd269bab69e78111357ba1c735e75e
juunhyuukk <65065716+juunhyuukk@users.noreply.github.com>
2023-02-09 14:52:43 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+A = input()
+B = input()
+
+Alst = []
+Blst = []
+
+for i in A :
+    Alst.append(i)
+for i in B :
+    Blst.append(i)
+
+print(int(A)*int(Blst[2]))
+print(int(A)*int(Blst[1]))
+print(int(A)*int(Blst[0]))
+print(int(A)*int(B))
